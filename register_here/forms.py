b663ce11e06f65a80a4b32daf26639efec897ed2
Daniel <daniel.anayavasquez@sjsu.edu>
2025-04-25 19:04:36 -0700

    Project update

---
+++ register_here/forms.py
@@ -0,0 +1,25 @@
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RecipeForm(FlaskForm):
+    title = StringField('Recipe Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit Recipe')
+
+class VisitorEmailForm(FlaskForm):
+    email = StringField('Enter your Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('View Recipes')
